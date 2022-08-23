5b003d24feedcc9876fd306c1aca73ec39c1a044
Nguyen Quyen <tomcon981@gmail.com>
2022-08-23 09:25:19 +0000

    answer valid parentheses

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -2,6 +2,27 @@
  * @param {string} s
  * @return {boolean}
  */
- var isValid = function(s) {
-    
+var isValid = function (s) {
+  const match = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+  };
+  const hashMap = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (match[s[i]]) {
+      hashMap.push(s[i]);
+    } else {
+      if (s[i] === match[hashMap[hashMap.length - 1]]) {
+        hashMap.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
 };
+
+isValid("((({[}[]})))");
+console.log(isValid("((({[][]})))"));
