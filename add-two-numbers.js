e383c1fd17233e92d7a1e0578a6f8c6bc36d3880
Nguyen <tomcon981@gmail.com>
2022-09-11 15:35:32 +0700

    new solution add two numbers

--- add-two-numbers.js
+++ add-two-numbers.js
@@ -16,29 +16,53 @@ function ListNode(val, next) {
   this.next = next === undefined ? null : next;
 }
 
-var addTwoNumbers = function (list1, list2) {
-  const list3 = new ListNode();
-  let prev = list3;
-  let surplus = 0;
-  let sum = 0;
+// var addTwoNumbers = function (list1, list2) {
+//   const list3 = new ListNode();
+//   let prev = list3;
+//   let surplus = 0;
+//   let sum = 0;
 
-  while (list1 || list2) {
-    sum = (list1?.val || 0) + (list2?.val || 0) + surplus;
-    if (sum >= 10) {
-      prev.next = new ListNode(sum - 10);
-      surplus = 1;
-    } else {
-      surplus = 0;
-      prev.next = new ListNode(sum);
+//   while (list1 || list2) {
+//     sum = (list1?.val || 0) + (list2?.val || 0) + surplus;
+//     if (sum >= 10) {
+//       prev.next = new ListNode(sum - 10);
+//       surplus = 1;
+//     } else {
+//       surplus = 0;
+//       prev.next = new ListNode(sum);
+//     }
+//     list1 = list1?.next || 0;
+//     list2 = list2?.next || 0;
+//     prev = prev.next;
+//   }
+//     if (surplus) {
+//         prev.next = new ListNode(1);
+//     }
+//   return list3.next;
+// };
+
+var addTwoNumbers = function (list1, list2) {
+  const listnode = new ListNode();
+  let head = listnode;
+  let carry = 0;
+
+  while (list1 || list2 || carry) {
+    let sum = carry;
+    if (list1) {
+      sum += list1.val;
+      list1 = list1.next;
     }
-    list1 = list1?.next || 0;
-    list2 = list2?.next || 0;
-    prev = prev.next;
+    if (list2) {
+      sum += list2.val;
+      list2 = list2.next;
+    }
+
+    carry = Math.floor(sum / 10);
+    head.next = new ListNode(sum % 10);
+    head = head.next;
   }
-  if (surplus) {
-    prev.next = new ListNode(1);
-  }
-  return list3.next;
+
+  return listnode.next;
 };
 
 // create first linked list: 1 -> 3 -> 10
