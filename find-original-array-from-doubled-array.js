85d25fced628e09002a42103979d8331f8425e5a
Nguyen Quyen <tomcon981@gmail.com>
2022-09-16 16:53:07 +0000

    .

--- find-original-array-from-doubled-array.js
+++ find-original-array-from-doubled-array.js
@@ -6,12 +6,14 @@ var findOriginalArray = function (changed) {
   let result = [];
   let arr = changed;
 
+  arr.sort((a, b) => a - b);
+
   for (let i = 0; i < changed.length; i++) {
     let count = 1;
     while (arr && count <= arr.length) {
       if (arr[0] === arr[count] * 2 || arr[0] === arr[count] / 2) {
         result.push(arr[0] > arr[count] ? arr[count] : arr[0]);
-        arr = arr.filter((val, index) => index !== 0 && index !== count);
+        arr = arr.filter((_, index) => index !== 0 && index !== count);
         break;
       }
       count++;
@@ -25,4 +27,8 @@ var findOriginalArray = function (changed) {
 // findOriginalArray([1]);
 // console.log(findOriginalArray([6, 3, 0, 0, 4, 5]));
 console.log(findOriginalArray([1, 2, 2, 4, 1, 8]));
+console.log(findOriginalArray([4, 4, 16, 20, 8, 8, 2, 10]));
+console.log(findOriginalArray(
+  
+));
 // console.log(findOriginalArray([4, 4, 16, 20, 8, 8, 2, 10]));
