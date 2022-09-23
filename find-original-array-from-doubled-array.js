56cc821878d77fec54395baa172bb4e15d11b59e
Nguyen <tomcon981@gmail.com>
2022-09-23 21:22:36 +0700

    find original array from doubled array solution

--- find-original-array-from-doubled-array.js
+++ find-original-array-from-doubled-array.js
@@ -12,18 +12,19 @@ var findOriginalArray = function (changed) {
 
   changed.sort((a, b) => a - b);
 
-  while (changed.length > 0 && count <= changed.length) {
-    const firstEl = changed[0];
-    let iMatch = changed.indexOf(firstEl * 2, 1);
-
-    if (iMatch > 0) {
-      result.push(firstEl);
-      changed.splice(0, 1);
-      changed.splice(iMatch - 1, 1);
-      count = -1;
-    } else {
-      return [];
+  while (count < changed.length) {
+    if (changed[count] > -1) {
+      const firstEl = changed[count];
+      let iMatch = changed.indexOf(firstEl * 2, count + 1);
+      if (iMatch > 0) {
+        result.push(firstEl);
+        changed[count] = -1;
+        changed[iMatch] = -1;
+      } else {
+        return [];
+      }
     }
+    count++;
   }
 
   return result;
@@ -34,5 +35,5 @@ var findOriginalArray = function (changed) {
 // console.log(findOriginalArray([6, 3, 0, 0, 4, 5]));
 // console.log(findOriginalArray([1, 2, 2, 4, 1, 8]));
 // console.log(findOriginalArray([4, 4, 16, 20, 8, 8, 2, 10]));
-console.log(findOriginalArray([0, 0, 1, 0]));
+// console.log(findOriginalArray([0, 0, 0, 0]));
 console.log(findOriginalArray([1, 3, 4, 2, 6, 8]));
